1df4ec54bb4f1c879ed5ee11165dd459cce28f7d
Michael Ben-Nes <miki@epoch.co.il>
2025-08-10 11:36:18 +0300

    Fix logo under dark mode

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,7 @@
 import siteMetadata from '@/data/siteMetadata'
 import headerNavLinks from '@/data/headerNavLinks'
 import Logo from '@/data/logo.svg'
+import LogoDark from '@/data/logo-dark.svg'
 import Link from './Link'
 import MobileNav from './MobileNav'
 import ThemeSwitch from './ThemeSwitch'
@@ -16,8 +17,11 @@ const Header = () => {
     <header className={headerClass}>
       <Link href="/" aria-label={siteMetadata.headerTitle}>
         <div className="flex items-center justify-between">
-          <div className="mr-3">
+          <div className="mr-3 dark:hidden">
             <Logo />
+          </div>
+          <div className="mr-3 hidden dark:block">
+            <LogoDark />
           </div>
           {typeof siteMetadata.headerTitle === 'string' ? (
             <div className="hidden h-6 text-2xl font-semibold sm:block">
